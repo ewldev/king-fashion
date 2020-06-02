8235947ac1cc5323cf7d5fc24b31c0998e9f1f09
eleung32 <52718704+eleung32@users.noreply.github.com>
2020-06-02 17:14:08 -0400

    implement code that pulls in our shop data from firestore and stores it into the shop reducer from our shop page. Also included our withSpinner HOC

---
+++ src/pages/shop/shop.component - Copy.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+import CollectionsOverview  from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
+
+const ShopPage = ({ match }) => (
+    <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>    
+);
+
+export default ShopPage;
+// const ShopPage = ({ match }) => {
+//     console.log(match);
+//     return (
+//         <div className='shop-page'>
+//             <Route exact path={`${match.path}`} component={CollectionsOverview} />
+//         </div>    
+//     );
+// }
+
+
