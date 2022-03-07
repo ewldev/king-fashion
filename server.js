78f0f2dff5766dac4f78bb4317151cc7f45c9fc0
eleung32 <52718704+eleung32@users.noreply.github.com>
2022-03-06 22:30:50 -0500

    modified server app.get

--- server.js
+++ server.js
@@ -15,14 +15,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use(cors());
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
 
-  // app.get('*', function(req, res) { //from https://github.com/ZhangMYihua/lesson-32-complete
-  app.get('/*', function (req, res) {  
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {  
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
-}
+// }
 
 app.listen(port, error => {
   if (error) throw error;
